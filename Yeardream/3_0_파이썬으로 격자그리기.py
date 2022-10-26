ad58db25b17a572031cd224cf925a1e6650dea4b
municef1 <esemoon@gmail.com>
2022-10-26 20:36:20 +0900

    organize folders

--- Yeardream/3_0_파이썬으로 격자그리기.py
+++
@@ -1,8 +0,0 @@
-# matplotlib 설치해야함
-#
-# win10 기준 cmd로 파이썬 인식시키기 https://wxmin.tistory.com/121
-
-# anaconda prompt 켜고python get-pip.py
-# python -m pip install -U pip
-# python -m pip install -U matplotlibpython get-pip.py
-
--- study/2.py
+++
@@ -1,8 +0,0 @@
-# \n 사용
-multiline = "Life is too short\nYou need python"
-
-# 따옴표 사용
-multiline = '''
-Life is too short
-You need python
-'''
